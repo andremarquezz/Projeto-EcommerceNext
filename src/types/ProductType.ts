de50c9369b7c1398868e90394b847915ed3fe8f6
Jey <andre.marqquez@gmail.com>
2024-02-05 07:54:14 -0300

    Cria classe Produto

---
+++ src/types/ProductType.ts
@@ -0,0 +1,10 @@
+type ProductType = {
+  id: number;
+  title: string;
+  price: number | null;
+  image: string;
+  description: string | null;
+  category: string;
+};
+
+export default ProductType
